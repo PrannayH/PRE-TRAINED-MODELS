ec00989c5b4205f61c51563d5356b72c1c10ed29
Praadnya <99022986+Praadnya@users.noreply.github.com>
2023-12-26 12:34:21 +0530

    Add files via upload

---
+++ QuestionAnswering/roberta_base_qa_ui.py
@@ -0,0 +1,28 @@
+import gradio as gr
+from transformers import pipeline
+
+# Load the question-answering pipeline with 'roberta-base' model
+qa_pipeline = pipeline('question-answering', model='roberta-base', tokenizer='roberta-base')
+
+def answer_question(context, question=""):
+    # Default question if left empty
+    if not question:
+        question = "What is the main idea?"
+
+    # Perform question answering
+    result = qa_pipeline(context=context, question=question)
+    return result['answer']
+
+iface = gr.Interface(
+    fn=answer_question,
+    inputs=[
+        gr.Textbox(placeholder="Enter context...", label="Context"),
+        gr.Textbox(placeholder="Enter question...", label="Question")
+    ],
+    outputs=gr.Textbox(placeholder="Answer will be displayed here...", label="Answer"),
+    live=True,
+    title="RoBERTa Question Answering",
+    description="Enter a context and a question, then click Submit to get the answer.",
+)
+
+iface.launch()
