b853aa3f6ac78344d09e9d8814f2fc10ed788c5e
Praadnya <99022986+Praadnya@users.noreply.github.com>
2024-01-04 16:28:16 +0530

    Add files via upload

---
+++ SentimentAnalysis/roberta_sa_ui.py
@@ -0,0 +1,34 @@
+import gradio as gr
+from transformers import pipeline
+
+# Load the sentiment analysis pipeline with 'roberta-base' model
+sentiment_pipeline = pipeline('sentiment-analysis', model='roberta-base', tokenizer='roberta-base')
+
+def analyze_sentiment(text):
+    # Perform sentiment analysis
+    result = sentiment_pipeline(text)
+    sentiment_label = result[0]['label']
+    
+    # Print the actual label
+    print(f"Actual label: {sentiment_label}")
+
+    # Map labels to more understandable terms
+    if sentiment_label == 'LABEL_0':
+        sentiment = 'Negative'
+    elif sentiment_label == 'LABEL_1':
+        sentiment = 'Positive'
+    else:
+        sentiment = 'Neutral'  # You can add more cases if needed
+    
+    return sentiment
+
+iface = gr.Interface(
+    fn=analyze_sentiment,
+    inputs=gr.Textbox(placeholder="Enter text...", label="Text"),
+    outputs=gr.Textbox(placeholder="Sentiment will be displayed here...", label="Sentiment"),
+    live=True,
+    title="RoBERTa Sentiment Analysis",
+    description="Enter a text, then click Submit to get the sentiment analysis result.",
+)
+
+iface.launch()
